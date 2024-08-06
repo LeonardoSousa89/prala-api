8a2615a202afff5d6980ef834ba1d5df086a012c
LeonardoSousa89 <leoprofessionallogytech@outlook.com>
2024-08-05 22:40:19 -0300

    sixth commit - identifying error

--- security/cors/cors.js
+++ security/cors/cors.js
@@ -1,13 +1,20 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.policy = void 0;
 const cors_1 = __importDefault(require("cors"));
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
 // https://developer.mozilla.org/en-US/docs/Web/HTTP/Headers
 const policy = (0, cors_1.default)({
-    origin: ["https://pralalogistica.com.br", "http://pralalogistica.com.br"],
+  origin: [
+    "https://pralalogistica.com.br",
+    "http://pralalogistica.com.br",
+    "http://localhost:5500",
+    "http://127.0.0.1:5500",
+  ],
 });
 exports.policy = policy;
